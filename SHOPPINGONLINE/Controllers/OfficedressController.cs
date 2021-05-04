25213b837c2684da661b90019240ab36ccdd098e
nttvyy <nttvyy25@gmail.com>
2021-05-04 16:12:49 +0700

    GD

---
+++ SHOPPINGONLINE/Controllers/OfficedressController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHOPPINGONLINE.Controllers
+{
+    public class OfficedressController : Controller
+    {
+        // GET: Officedress
+        public ActionResult Officedress()
+        {
+            return View();
+        }
+    }
+}
